7efbbd84bc1080c556fb4d70157471761b93e71d
alka345 <alkaraojnv@gmail.com>
2024-01-27 12:22:18 +0530

    feat

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+// import { ReactDOM } from 'react'
 import ReactDOM from 'react-dom'
 // import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
